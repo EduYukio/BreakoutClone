d236fa68f7bb84dc35c9913245cfbed3df9807e8
Ana <ana.mayumi.12@gmail.com>
2021-08-19 23:18:23 -0300

    Test commit, changing things

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Ball : MonoBehaviour {
+public class Ball : MonoBehaviour
+{
     public Rigidbody2D rb;
-    public float speed = 5;
+    public float speed = 8;
 
-    void Awake() {
+    void Awake()
+    {
         rb = GetComponent<Rigidbody2D>();
 
         Vector3 dir = new Vector3(-0.3f, -1f, 0).normalized;
@@ -15,37 +17,46 @@ public class Ball : MonoBehaviour {
         rb.velocity = dir * speed;
     }
 
-    void Update() {
-        if (transform.position.y < -7) {
+    void Update()
+    {
+        if (transform.position.y < -7)
+        {
             SceneManager.LoadScene("SampleScene");
         }
     }
 
-    private void invertXDir(Rigidbody2D rb) {
+    private void invertXDir(Rigidbody2D rb)
+    {
         Vector3 currentVelocity = rb.velocity;
         Vector3 newVelocity = new Vector3(-1 * currentVelocity.x, currentVelocity.y, 0);
         rb.velocity = newVelocity;
     }
 
-    private void invertYDir(Rigidbody2D rb) {
+    private void invertYDir(Rigidbody2D rb)
+    {
         Vector3 currentVelocity = rb.velocity;
         Vector3 newVelocity = new Vector3(currentVelocity.x, -1 * currentVelocity.y, 0);
         rb.velocity = newVelocity;
     }
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnCollisionEnter2D(Collision2D other)
+    {
         // private void OnTriggerEnter2D(Collision2D other) {
         string otherTag = other.gameObject.tag;
-        if (otherTag == "Player" || otherTag == "Ceiling") {
+        if (otherTag == "Player" || otherTag == "Ceiling")
+        {
             invertYDir(rb);
         }
-        else if (otherTag == "Wall") {
+        else if (otherTag == "Wall")
+        {
             invertXDir(rb);
         }
-        else if (otherTag == "BlockLeft" || otherTag == "BlockRight") {
+        else if (otherTag == "BlockLeft" || otherTag == "BlockRight")
+        {
             invertXDir(rb);
             Destroy(other.transform.parent.gameObject);
         }
-        else if (otherTag == "BlockTop" || otherTag == "BlockBot") {
+        else if (otherTag == "BlockTop" || otherTag == "BlockBot")
+        {
             invertYDir(rb);
             Destroy(other.transform.parent.gameObject);
         }
